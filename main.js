ae2cd25e7ed6fbe1a9657b189a2db11fd3cc0a2b
ismaelsalm <ismaelsalm42@gmail.com>
2026-01-27 03:33:43 -0300

    Add human interaction function

--- main.js
+++ main.js
@@ -9,4 +9,19 @@ function getComputerChoice() {
     return randomFactor > 66 ? Option.Rock : randomFactor > 33 ? Option.Paper : Option.Scissors;
 }
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+    const option = prompt("Choose Rock, Paper or Scissors.", Option.Rock).toLowerCase()
+    if(option == "r") return Option.Rock;
+    if(option == "p") return Option.Paper;
+    if(option == "s") return Option.Scissors;
+
+    if(option == "rock") return Option.Rock;
+    if(option == "paper") return Option.Paper;
+    if(option == "scissors") return Option.Scissors;
+
+    if(option == "1") return Option.Rock;
+    if(option == "2") return Option.Paper;
+    if(option == "3") return Option.Scissors;
+}
+
+console.log(getHumanChoice())
